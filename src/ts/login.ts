b1bf5520903c6ffc20c8a249bac43de06095c81e
josemesa <josemesamarin@gmail.com>
2024-04-25 22:58:44 +0200

    jose.messa/Edit HTML to javascript Inner in DOM

---
+++ src/ts/login.ts
@@ -0,0 +1,21 @@
+export function loginPage(){
+    const pageElement: any = document.getElementById("app")
+    pageElement.innerHTML=`<div class="main-section">
+    <div class="login-box">
+      <img class="logo-login" src="nexiatransp.png" alt="">
+      
+      <form class="login-form" action="submit" method="POST">
+        <div class="email-box"><label for="email">Email:</label>
+        <input type="email" id="email" name="email" placeholder="Escribe aqui tu email" required>
+      </div>
+       <div class="pass-box"><label for="password">Contraseña:</label>
+        <input type="password" id="password" name="password" placeholder="Escribe aqui tu contraseña" required>
+      </div> 
+        <input class="login-button" type="submit" value="Login">
+        <div class="suscribe"><p>Aun no disfrutas de nuestras ventajas?</p><a href="#">Suscribete aquí</a></div>
+    </form>
+
+    </div>
+    </div>`    
+}
+
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,17 +1,2 @@
-// // html code
-
-// <header>
-// <div class="header-bar">
-//   <img src="logo-2.png" alt="">
-// </div>
-// </header>
-// <main>
-// <div>
-//   <div class="main-control-box">
-//   <div class="main-control">
-//       <h2 class="tools">TOOLS</h2>
-//   </div>
-// </div>
-// </div>
-// </main>
-// <footer></footer>
+import { loginPage } from "./login";
+loginPage()
