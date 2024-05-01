7b56f4d586f0fd52de52202849c3432fabbcf052
davidgm05 <davidgutierrezmillan05@gmail.com>
2024-05-01 14:28:48 +0200

    David.Gutierrez/insert the html with user information and format the birthdate at a readable time

--- src/ts/getMyUser.js
+++ src/ts/getMyUser.js
@@ -1,28 +1,57 @@
 export function getMyUser() {
-    const authToken = localStorage.getItem("token");
+  const authToken = localStorage.getItem("token");
 
-    if (!authToken) {
-      console.error(
-        "No se encontró un token de autenticación. Inicie sesión para obtener acceso."
-      );
-      return;
-    }
-  
-    fetch("http://localhost:3000/user/getmyuser", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "auth-token": authToken,
-      },
-    })
+  if (!authToken) {
+    console.error(
+      "No se encontró un token de autenticación. Inicie sesión para obtener acceso."
+    );
+    return;
+  }
+
+  fetch("http://localhost:3000/user/getmyuser", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "auth-token": authToken,
+    },
+  })
     .then((response) => {
-        if(!response.ok){
-            return new Error(`Error: ${response.statusText}`);
-        }
-        return response.json();
+      if (!response.ok) {
+        return new Error(`Error: ${response.statusText}`);
+      }
+      return response.json();
     })
     .then((data) => {
-        const myUser = data.data;
-        console.log("Datos del usuario:", myUser);
-    })
+      const myUser = data.data;
+      console.log("Datos del usuario:", myUser);
+
+      const birthdate = new Date(myUser.birthdate);
+      const options = {
+        timeZone: "Europe/Madrid",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      };
+
+      const formattedBirthdate = birthdate.toLocaleString("en-US", options);
+      console.log(formattedBirthdate);
+
+      const html = `<div class="div-my-user">
+        <div class="firstname">${myUser.firstname}</div>
+        <div class="my-surname">${myUser.surname}</div>
+        <div class="email">${myUser.email}</div>
+        <div class="user-id">${myUser._id}</div>
+        <div class="birthdate">${formattedBirthdate}</div>
+        <div id="my-subscription">${myUser.subscription}</div>
+      </div>`;
+
+      console.log(html);
+
+      const containerMyUser = document.getElementById("container-my-user");
+
+      containerMyUser.innerHTML = html;
+    });
 }
--- src/ts/userPage.js
+++ src/ts/userPage.js
@@ -1,23 +1,29 @@
 import { getMyUserlistener } from "./events/myUser";
 
-export function userPage(userName, userSurname){  
-  const pageElement= document.getElementById("app")
-  pageElement.innerHTML=""
-  pageElement.innerHTML=`<header>
+export function userPage(userName, userSurname) {
+  const pageElement = document.getElementById("app");
+  pageElement.innerHTML = "";
+  pageElement.innerHTML = `<header>
   <div class="header-bar">
   <img src="nexiatransp.png" alt=""><div class="user-line"><h2 class="page-title">USER DASHBOARD</h2><h3>User: ${userName} ${userSurname}</h3></div>
   </div>
   </header>
   <main>
-  <div>
-    <div class="main-control-box">
-    <div class="main-control">
-    <h3 class="my-user"><a href="#" id="my-user">My users</a></h3>
-       
+  <div class="body-app">
+      <div class="main-control-box">
+      <div class="main-control">
+      <h3 class="my-user"><a href="#" id="my-user">Users</a></h3>
+      </div>
+      </div>
+      <div class="container-my-user" id="container-my-user">
+      <div class="welcome">
+        <p class="welcome-message">Welcome to <b>Nexiad Web User</b></h2>
+        <p class="page-message">In this app you can manage, consult your user.</p>
+        <p></p>
+        </div>
+      </div>
     </div>
-  </div>
-  </div>
   </main>
-  <footer></footer>`
+  <footer></footer>`;
   getMyUserlistener();
 }
