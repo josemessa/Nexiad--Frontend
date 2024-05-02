19bd9a0815e94416ee09ff4e7ffc005428dae9bf
davidgm05 <davidgutierrezmillan05@gmail.com>
2024-05-02 12:26:11 +0200

    David.Gutierrez/continuing with the profile page styles

--- src/ts/getMyUser.js
+++ src/ts/getMyUser.js
@@ -39,13 +39,26 @@ export function getMyUser() {
       const formattedBirthdate = birthdate.toLocaleString("en-US", options);
       console.log(formattedBirthdate);
 
+      const color = getRandomColor(); // Función para generar un color aleatorio
+      const initial = myUser.firstname.charAt(0); // Obtener la primera letra del nombre
+
       const html = `<div class="div-my-user">
-        <div class="firstname">${myUser.firstname}</div>
-        <div class="my-surname">${myUser.surname}</div>
-        <div class="email">${myUser.email}</div>
-        <div class="user-id">${myUser._id}</div>
-        <div class="birthdate">${formattedBirthdate}</div>
-        <div id="my-subscription">${myUser.subscription}</div>
+        <div class="container-name">
+        <div class="profile-image" style="background-color: ${color};">
+          ${initial}
+        </div>
+          <div class="firstname">${myUser.firstname}</div>
+          <div class="my-surname">${myUser.surname}</div>
+        </div>
+        <div class="container-email">
+          <div class="email">${myUser.email}</div>
+          <div class="user-id">${myUser._id}</div>
+        </div>
+        <div class"container-adress">
+          <div class="adress"><p>Calle de la Luna, 15<br>28001 Madrid, Spain</p></div>
+          <div class="birthdate">${formattedBirthdate}</div>
+        </div>
+        <div id="my-subscription" class="${myUser.subscription}">${myUser.subscription}</div>
       </div>`;
 
       console.log(html);
@@ -55,3 +68,21 @@ export function getMyUser() {
       containerMyUser.innerHTML = html;
     });
 }
+
+function getRandomColor() {
+  const colors = [
+    "#3498db",
+    "#2ecc71", 
+    "#f1c40f", 
+    "#e67e22", 
+    "#e74c3c", 
+    "#9b59b6", 
+    "#1abc9c", 
+    "#16a085", 
+    "#f39c12",
+    "#c0392b", 
+  ];
+
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
