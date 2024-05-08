1b02914e8f81c545e8b93d1392cf375643b1b04c
davidgm05 <davidgutierrezmillan05@gmail.com>
2024-05-08 21:45:21 +0200

    David.Gutierrez/finishing the listening event with the injection of the form with its data, the connection and the recovery of the getUserById html with the updated data and the functionality of the other buttons before and after using the edit button

--- src/ts/patchUser.js
+++ src/ts/patchUser.js
@@ -1,97 +1,112 @@
 import { deleteListener } from "./events/delete";
 import { disableAdminAccessListener, disableAccessListener } from "./events/disable"
-import { patchUserByIdListener } from "./events/patch"
 
-export function getUserById(userId) {
-  const authToken = localStorage.getItem("token");
+export function patchUser(userId) {
+    const firstname = document.getElementById("firstname").value;
+    const surname = document.getElementById("surname").value;
+    const adress = document.getElementById("adress").value;
+    const city = document.getElementById("city").value;
+    const phone = document.getElementById("phone").value;
+    const email = document.getElementById("email").value;
+    const birthdate = document.getElementById("birthdate").value;
 
-  if (!authToken) {
-    console.error(
-      "No se encontró un token de autenticación. Inicie sesión para obtener acceso."
-    );
-    return;
-  }
+    const authToken = localStorage.getItem("token");
 
-  fetch(`http://localhost:3000/user/${userId}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      "auth-token": authToken,
-    },
-  })
+    if (!authToken) {
+        console.error(
+            "No se encontró un token de autenticación. Inicie sesión para obtener acceso."
+        );
+        return;
+    }
+
+    let user;
+
+    fetch(`http://localhost:3000/user/${userId}/edituser`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            "auth-token": authToken,
+        },
+        body: JSON.stringify({
+            firstname: firstname,
+            surname: surname,
+            adress: adress,
+            city: city,
+            phone: phone,
+            email: email,
+            birthdate: birthdate,
+        })
+    })
     .then((response) => {
-      if (!response.ok) {
-        return new Error(`Error: ${response.statusText}`);
-      }
-      return response.json();
+        if (!response.ok) {
+            return new Error(`Error: ${response.statusText}`);
+        }
+        return response.json();
     })
     .then((data) => {
-      const userData = data.data;
-      console.log("Usuario obtenido por ID:", userData);
-
-      // Generar el HTML con la información del usuario obtenida
-      const birthdate = new Date(userData.birthdate);
-      const options = {
-        timeZone: "Europe/Madrid",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      };
-
-      const colorMap = {
-        A: "#3498db",
-        B: "#2ecc71",
-        C: "#f1c40f",
-        D: "#e67e22",
-        E: "#e74c3c",
-        F: "#9b59b6",
-        G: "#1abc9c",
-        H: "#16a085",
-        I: "#f39c12",
-        J: "#c0392b",
-        K: "#8e44ad",
-        L: "#34495e",
-        M: "#27ae60",
-        N: "#2980b9",
-        O: "#d35400",
-        P: "#2c3e50",
-        Q: "#f7dc6f",
-        R: "#7f8c8d",
-        S: "#16a085",
-        T: "#e74c3c",
-        U: "#95a5a6",
-        V: "#1abc9c",
-        W: "#f1c40f",
-        X: "#9b59b6",
-        Y: "#8e44ad",
-        Z: "#3498db",
-      };
-
-      const initial = userData.firstname.charAt(0).toUpperCase();
-      const color = colorMap[initial] || getRandomColor();
-
-      const formattedBirthdate = birthdate.toLocaleString("en-US", options);
-
-      const html = `<div class="user-edit" id="user-edit">
+        user = data.data; 
+        console.log(`se actualizo correctamente`);
+        const birthdate = new Date(user.birthdate);
+        const options = {
+          timeZone: "Europe/Madrid",
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        };
+  
+        const colorMap = {
+          A: "#3498db",
+          B: "#2ecc71",
+          C: "#f1c40f",
+          D: "#e67e22",
+          E: "#e74c3c",
+          F: "#9b59b6",
+          G: "#1abc9c",
+          H: "#16a085",
+          I: "#f39c12",
+          J: "#c0392b",
+          K: "#8e44ad",
+          L: "#34495e",
+          M: "#27ae60",
+          N: "#2980b9",
+          O: "#d35400",
+          P: "#2c3e50",
+          Q: "#f7dc6f",
+          R: "#7f8c8d",
+          S: "#16a085",
+          T: "#e74c3c",
+          U: "#95a5a6",
+          V: "#1abc9c",
+          W: "#f1c40f",
+          X: "#9b59b6",
+          Y: "#8e44ad",
+          Z: "#3498db",
+        };
+  
+        const initial = user.firstname.charAt(0).toUpperCase();
+        const color = colorMap[initial] || getRandomColor();
+  
+        const formattedBirthdate = birthdate.toLocaleString("en-US", options);
+        const html = `<div class="user-edit" id="user-edit">
             <div class="div-my-user" id="div-my-user">
                 <div class="container-name">
                     <div class="profile-image" style="background-color: ${color};">
                         ${initial}
                     </div>
-                    <div class="firstname">${userData.firstname}</div>
-                    <div class="my-surname">${userData.surname}</div>
-                    <div class="subscription-name">${userData.subscription}</div>
+                    <div class="firstname">${user.firstname}</div>
+                    <div class="my-surname">${user.surname}</div>
+                    <div class="subscription-name">${user.subscription}</div>
                 </div>
                 <div class="container-email">
-                    <div class="email"><b>email:  </b>${userData.email}</div>
-                    <div class="numero"><b>numero:  </b>${userData.phone}</div>
+                    <div class="email"><b>email:  </b>${user.email}</div>
+                    <div class="numero"><b>numero:  </b>${user.phone}</div>
                 </div>
                 <div class="container-id-rol">
-                <div class="user-id" id="user-id"><b>id:  </b>${userData._id}</div>
-                <div class="user-rol" id="user-rol"><b>rol:  </b>${userData.role}</div>
+                <div class="user-id" id="user-id"><b>id:  </b>${user._id}</div>
+                <div class="user-rol" id="user-rol"><b>rol:  </b>${user.role}</div>
                 </div>
                 <div class="container-adress">
-                    <div class="adress"><b>direccion:</b>  ${userData.adress}</div>
+                    <div class="adress"><b>direccion:</b>  ${user.adress}</div>
                     <div class="birthdate"><b>fecha de nacimiento:</b>  ${formattedBirthdate}</div>
                 </div>
             </div>
@@ -105,19 +120,13 @@ export function getUserById(userId) {
 </g>
 </svg>
             </div>`;
-
-      const containerMyUser = document.getElementById("aplication-box");
-      containerMyUser.innerHTML = html;
-      deleteListener(userData._id)
-      disableAdminAccessListener(userData._id)
-      disableAccessListener(userData._id)
-      patchUserByIdListener(userData._id)
-      
-    }
-    
-  )
-    
+    const userContainer = document.getElementById("user-edit");
+    userContainer.innerHTML = html;
+    deleteListener(user._id)
+    disableAdminAccessListener(user._id)
+    disableAccessListener(user._id)
+    })
     .catch((error) => {
-      console.error("Hubo un problema con la solicitud:", error);
+        console.error("Error al actualizar el usuario:", error);
     });
 }
