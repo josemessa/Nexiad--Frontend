e245b6472d7b365c9815e724c088c786f77db9c4
josemesa <josemesamarin@gmail.com>
2024-05-13 23:59:51 +0200

    jose.messa/EDIT styles in registers

--- src/ts/events/login.js
+++ src/ts/events/login.js
@@ -77,8 +77,6 @@ export function subscribeEventlistener() {
   const subscribe = document.getElementById("subscribe");
   let subscriptionsData;
 
-  
-
   subscribe.addEventListener("click", (event) => {
     const loginBox = document.getElementById("login-box");
     loginBox.innerHTML = "";
@@ -90,40 +88,40 @@ export function subscribeEventlistener() {
       </div>
         <form class="user-form-login" action="submit" method="POST">
         <div >
-        <label>Nombre:</label>
+        <label>Nombre:</label><br>
         <input type="firstname" id="firstname" placeholder="ej. Jose Antonio" required>
     </div>
     <div >
-        <label>Apellidos:</label>
+        <label>Apellidos:</label><br>
          <input type="surname" id="surname"  placeholder="ej. Plaza Martinez" required>
     </div>
     <div>
-        <label>Direccion:</label>
+        <label>Direccion:</label><br>
          <input type="adress" id="adress" placeholder="ej. Avda. Los Alamos">
     </div>
     <div>
-        <label>Ciudad:</label>
+        <label>Ciudad:</label><br>
          <input type="city" id="city"  placeholder="ej. Málaga">
     </div>           
     <div>
-        <label>Numero de telefono:</label>
+        <label>Numero de telefono:</label><br>
          <input type="phone" id="phone" placeholder="+34 6XX XXX XXX" >
     </div>           
     <div>
-        <label for="birthdate">Fecha de Nacimiento:</label>
+        <label for="birthdate">Fecha de Nacimiento:</label><br>
         <input type="date" id="birthdate" required>
     </div>
    
-        <div><label >Email:</label>
+        <div><label >Email:</label><br>
         <input type="email" id="email" name="email" placeholder="Escribe aqui tu email" required>
     </div>
     <div>
-        <label for="password">Contraseña:</label>
+        <label for="password">Contraseña:</label><br>
         <input type="password" id="password" name="password" placeholder="Escribe aqui tu contraseña" required>
         <span id="password-error" style="color: red; display: none;">La contraseña debe tener al menos 8 caracteres y un número</span>
     </div>
     <div>
-        <label for="password">Repite tu contraseña</label>
+        <label for="password">Repite tu contraseña</label><br>
         <input type="password" id="password-confirmation" name="password" placeholder="Repite aqui la contraseña" >
     </div>
     <div id="container-select-subscription"></div>
@@ -144,32 +142,34 @@ export function subscribeEventlistener() {
     backToLogin();
 
     fetch(`http://localhost:3000/subscription/getsubscriptions`, {
-            method: "GET",
-            headers: {
-                "content-type": "application/json"
-            },
-        })
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(`Error: ${response.statusText}`);
-            }
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data.data);
-            subscriptionsData = data.data;
-            const containerSelectSubscriptions = document.getElementById("subscription-select")
-            if(containerSelectSubscriptions){
-              subscriptionsData.forEach(subscription => {
-                const option = document.createElement("option")
-                option.value = subscription._id;
-                option.textContent = subscription.nombre;
-                containerSelectSubscriptions.appendChild(option);
-              })
-            }
-        })
-        .catch((error) => {
-            console.error(`Hubo un problema con la solicitud: ${error}`);
-        });
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Error: ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data.data);
+        subscriptionsData = data.data;
+        const containerSelectSubscriptions = document.getElementById(
+          "subscription-select"
+        );
+        if (containerSelectSubscriptions) {
+          subscriptionsData.forEach((subscription) => {
+            const option = document.createElement("option");
+            option.value = subscription._id;
+            option.textContent = subscription.nombre;
+            containerSelectSubscriptions.appendChild(option);
+          });
+        }
+      })
+      .catch((error) => {
+        console.error(`Hubo un problema con la solicitud: ${error}`);
+      });
   });
 }
